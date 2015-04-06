8dd96258a8c7da9d03f62d6dcfc1524331898a67
Anastasia Gaydashenko <a.v.gaydashenko@gmail.com>
2015-04-06 18:40:15 +0300

    add Sample3

---
+++ Sample3/mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QPushButton* btn = new QPushButton(this);
+    btn->setText("smth");
+    btn->setGeometry(5, 5, 100, 100);
+}
